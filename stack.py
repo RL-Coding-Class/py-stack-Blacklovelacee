753cad4ff618ac251ebd0a5cd02b517cf5965882
Maciej Šałyhin <92919825+Blacklovelacee@users.noreply.github.com>
2024-06-06 10:30:55 +0200

    Update stack.py

--- stack.py
+++ stack.py
@@ -1,19 +1,25 @@
 class Stack:
     """Provides stack container"""
     def __init__(self):
-        pass
-
-    def push(self, data):
-        pass
-
+        self._items = []
+        
+    def push(self, data) -> None:
+        self._items.append(data)
+    
     def pop(self):
-        pass
-
+        if self._items == None:
+            return None
+        temp = self._items[-1]    
+        del self._items[-1]
+        return temp
+    
     def get(self):
-        pass
-
-    def length(self):
-        pass
+        if self._items == None:
+            return None
+        return self._items[-1]
+    
+    def length(self) -> int:
+        return len(self._items)
 
     def __str__(self):
         pass
